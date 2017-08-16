aec461f462ba3b84d8da24b13dc99f9ce5dcb28a
Steve <sturner1123@gmail.com>
2017-08-16 17:52:42 -0400

    added keymap outline for default gherkin
    not correct

--- keyboards/gherkin/keymaps/default/keymap.c
+++ keyboards/gherkin/keymaps/default/keymap.c
@@ -2,12 +2,20 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+	/* Qwerty
+ 	* ,---------------------------------------------------------------------.
+ 	* | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |
+ 	* |------+------+------+------+------+------+------+------+------+------|
+ 	* | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |
+ 	* |------+------+------+------+------+------+------+------+------+------|
+ 	* | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |
+ 	* `---------------------------------------------------------------------'
+ 	*/
+
 	KEYMAP(
-		LT(1, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, 
-		KC_U, KC_I, KC_O, KC_P, KC_A, KC_S, 
-		KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, 
-		KC_L, KC_ESC, KC_Z, KC_X, KC_C, KC_V, 
-		KC_BSPC, KC_SPC, KC_B, KC_N, KC_M, KC_ENT),
+		LT(1, KC_Q), KC_W, KC_E, KC_R, KC_T   , KC_Y  , KC_U, KC_I, KC_O, KC_P, 
+		KC_A       , KC_S, KC_D, KC_F, KC_G   , KC_H  , KC_J, KC_K, KC_L, KC_ESC,
+		KC_Z       , KC_X, KC_C, KC_V, KC_BSPC, KC_SPC, KC_B, KC_N, KC_M, KC_ENT),
 
 	KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
